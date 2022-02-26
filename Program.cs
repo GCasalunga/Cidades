7930ac0114475718a178ea4728588eda5dc94d72
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-25 22:34:32 -0300

    finalizar atividade

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Guarujá");
